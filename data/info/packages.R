1279004d970b71b825c99614e253d5c1132078dd
Garrett Dancik <garrett.dancik@gmail.com>
2016-08-25 07:42:08 -0400

    Updates

--- data/info/packages.R
+++ data/info/packages.R
@@ -4,6 +4,9 @@
 ##    if working from a school computer
 ############################################################
 
+## for data.frame manipulation ##
+install.packages("dplyr")
+
 ## for knitting HTML documents ##
 install.packages("knitr")
 install.packages("yaml")
