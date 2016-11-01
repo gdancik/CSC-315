df69c32f1aa23fecbd391b8a683fceb9cdeef780
Garrett Dancik <garrett.dancik@gmail.com>
2016-11-01 15:05:14 -0400

    Update

--- data/info/packages.R
+++ data/info/packages.R
@@ -4,12 +4,29 @@
 ##    if working from a school computer
 ############################################################
 
-## for data.frame manipulation ##
-install.packages("dplyr")
-
 ## for knitting HTML documents ##
 install.packages("knitr")
 install.packages("yaml")
 install.packages("htmltools")
 install.packages("rmarkdown")
+
+## for permutation function ##
+install.packages("gtools")
+
+## for colorRampPalette function used to color heatmaps ##
+install.packages("RColorBrewer")
+
+#################################################
+# Bioconductor packages for Microarray Analysis
+#################################################
+
+## to install bioconductor packages, source the following once ##
+source("http://bioconductor.org/biocLite.R")
+
+biocLite("affy")
+biocLite("affydata")
+biocLite("leukemiasEset")
+biocLite("GEOquery")
+biocLite("limma")
+biocLite("hgu133a.db")
 
