292e03ddc8ee8a75a3253f42ce05a828bf226bae
Garrett Dancik <garrett.dancik@gmail.com>
2018-09-19 09:41:37 -0400

    Update

--- assignments.md
+++ assignments.md
@@ -12,12 +12,12 @@ exclude_from_nav: false
 * [Lab #1]({{ site.baseurl }}/data/hw/Lab1.R) (Due: Friday, 9/07/18) 
 * [Blackboard survey](https://easternct.blackboard.com/) (Due: Sunday, 9/9/18 by 5:00 PM)
 * [Lab #2]({{ site.baseurl }}/data/hw/Lab2.R) (Due: Monday, 9/17) 
+    * [Lab #2 Review]({{ site.baseurl }}/data/hw/Lab2-review.R) 
 <hr>
 * [Lab #3]({{ site.baseurl }}/data/hw/Lab3.pdf) (Due: Monday, 9/24) 
 {% comment %}
     * [Lab #1 Review]({{ site.baseurl }}/data/hw/Lab1-review.R) 
 * [Blackboard survey](https://ct-ecsu.blackboard.com/webapps/login/) (Due: Monday, 9/11)
-    * [Lab #2 Review]({{ site.baseurl }}/data/hw/Lab2-review.R) 
     * [Lab #3 Review]({{ site.baseurl }}/data/hw/Lab3-review.R) 
 * [Lab #4]({{ site.baseurl }}/data/hw/Lab4.R) (Due: Friday, 10/06) 
 * [Lab #5]({{ site.baseurl }}/data/hw/Lab5.R) (not collected) 
---
+++ data/hw/Lab2-review.R
@@ -0,0 +1,33 @@
+########################################################
+# Name: Review
+# CSC-315
+# Lab #2: Graphical and Numerical Summaries of Data
+########################################################
+
+library(readr)
+survey <- read_csv("https://gdancik.github.io/CSC-315/data/datasets/csc-315_survey_cleaned.csv")
+
+
+# 8. Out of the "Cat" people in this class, what has been their favorite
+#    CSC course so far? Answer this question by first creating a new
+#    data.frame for "Cat" people only. Then generate a relative frequency table
+#    for favorite programming language. Then answer the same question for "Dog" 
+#    people. What do you conclude about the favorite course for cat and dog people (for 
+#    students in this class) based on this data?
+
+
+
+# 9. Construct a histogram for Alchol consumption, by using the hist() function with the argument
+#    breaks = 14 to set the number of groupings. Describe the shape of its distribution. 
+#    Is it unimodal, bimodal, or flat. Is it skewed right, skewed left, or symmetric?
+
+
+# 10. Calculate the mean and median for Alcohol consumption. 
+#    Which is a better measure of averages? (Note: although these numbers are similar,
+#    one would still be considered better than the other -- why?)
+
+
+# 11. What is the 75th percentile for HS GPA??
+
+# 12. Ten percent of indivduals have HS GPAs above what value?
+
