627bf36da20fe184f611202d7a0a13f47447eaf9
Garrett Dancik <garrett.dancik@gmail.com>
2020-11-12 23:12:29 -0500

    Update

--- assignments.md
+++ assignments.md
@@ -21,8 +21,8 @@ exclude_from_nav: false
 * [Lab #8]({{ site.baseurl }}/data/hw/Lab8.R) (Due: Monday, 11/09/20) 
     * [Lab #8 results]({{ site.baseurl }}/data/hw/Lab8_results.pdf)  
 <hr>
+* [Lab #9]({{ site.baseurl }}/data/hw/Lab9.R) (Due: Friday, 11/20/20) 
 {% comment %} 
-* [Lab #9]({{ site.baseurl }}/data/hw/Lab9.R) (Due: Monday, 11/18/19) 
 * [Classification Challenge]({{ site.baseurl }}/data/hw/Challenge.pdf) (Due: see handout)  
     * [Challenge R Script]({{ site.baseurl }}/data/hw/Challenge.R)
 * [Blackboard survey](https://easternct.blackboard.com/) (Due: Sunday, 9/8/19 by 5:00 PM)
--- data/hw/Lab9.R
+++ data/hw/Lab9.R
@@ -14,98 +14,123 @@ library(GEOquery)
 library(ggplot2)
 library(dplyr)
 
-##################################################################################
-# 1.The code below reads in our class survey data and performs a 
+# 1) The code below reads in our class survey data and performs a 
 #   2-sample t-test to evaluate whether there is a statistically
-#   significant difference in Hours of Sleep between 'Cat' vs. 'Dog'
-#   people. Based on the code below, (a) find the p-value and state 
-#   your conclusion regarding the null hypothesis of H0: mu_cat - mu_dog = 0;
-#   and (b) calculate the difference in mean sleep between
-#   groups, using the formula: 
-#     mean hours of sleep for dog people - mean hours of sleep for cat people
-##################################################################################
-survey <- read.csv("https://gdancik.github.io/CSC-315/data/datasets/CSC-315_survey.csv")
+#   significant difference in the hours of sleep between 
+#   'Cat' and 'Dog' people. Based on the code below, 
+
+survey <- read.csv("https://gdancik.github.io/CSC-315/data/datasets/CSC315_survey_Fall_2020.csv")
 s <- split(survey$Sleep, survey$CatOrDogPerson)
 res <- t.test(s$Cat, s$Dog, var.equal = TRUE)
 
+#   (a) find the p-value and state your conclusion regarding the null 
+#       hypothesis of H0: mu_cat - mu_dog = 0
 
-##################################################################################
-# 2.Fit a linear model that predicts Hours of Sleep based on 
+
+#   (b) calculate the difference in mean hours of sleep between
+#   groups, using the formula: 
+#     mean hours of sleep for dog people - mean hours of sleep for cat people
+
+
+# 2) Fit a linear model that predicts Hours of Sleep based on 
 #   whether an individual is a cat or a dog person. You should use
 #   the treatment contrast where 'cat person' is the reference (x = 0) and 
 #   'dog person' is the treatment (x = +1)
-#    
+
+  
 # (a) Find and interpret the y-intercept of the regression line in the
 #      context of this problem.
+
 
 # (b) Find and interpret the slope of the regression line in the context of 
 #     this problem
 
+
 # (c) What is the p-value for the hypothesis test that there is a
 #     significant difference in Hours of Sleep between the two groups?
-#     (show this result in R, based on the linear model) Note: the p-value 
-#     from the linear model should match the p-value from the two-sample 
-#     t-test from problem 1(a) above.
-##################################################################################
+#     (show this result in R, based on the linear model). Note: the 
+#     p-value from the linear model should match the p-value from the
+#     two-sample t-test from problem 1(a) above.
+
+# 3) We can also fit a linear model using the 'sum' contrasts, which 
+#    is done below. This model has the form:
+#
+#    y = a + bx, where x = 1 for a Cat person and -1 for a Dog person
+
+fit <- lm(Sleep ~ CatOrDogPerson, data = survey, 
+          contrasts = list(CatOrDogPerson = 'contr.sum'))
+
+#  a) Show that the y-intercept is the average (mean) of the 
+#     the group means, i.e.,the y-intercept is equal to 
+#     [mean(sleep_cat_person) + mean(sleep_dog_person)] / 2
+#     by calculating and displaying this value, and also 
+#     displaying the y-intercept.
+
+#  b) Find and interpret the slope of the regression line in the 
+#     context of this problem
 
 
-###############################################################
-# 3. Get the processed data for GSE19143 and pull out the 
+# 4) Get the processed data for GSE19143 and extract the 
 #    expression data and phenotype data. Note that this
 #    dataset contains gene expression samples from children
 #    with Acute Lymphoblastic Leukemia (ALL), a cancer of
 #    the bone marrow. Tumor samples were treated with
-#    the anti-inflammatory drug prednisolone, and determined 
-#    to be either sensitive (responsive) or resistant 
-#    (non-responsive) to this drug. 
-###############################################################
+#    the anti-inflammatory drug prednisolone, and were
+#    determined to be either sensitive (responsive) or 
+#    resistant (non-responsive) to this drug. 
 
-# (a) How many samples had their gene expression values profiled?
+# (a) Use the expression matrix to find the number of samples that 
+#     had their gene expression values profiled.
 
-# (b) How many probes are on the array?
+
+# (b) Use the expression matrix to find the number of probes on the array.
+
 
 # (c) Take the log2 of the expression data, and generate a boxplot
 #     to show that the samples are properly processed and normalized.
-#     The analysis beginning with question 5 must use the log2 data; 
+#     The analysis beginning with question 6 must use the log2 data; 
 #     otherwise the results will not be correct.
 
 
-#####################################################################
-# 4.How many individuals are resistant to prednisolone and
-# how many are sensitive? 
-#####################################################################
+# 5) How many individuals are resistant to prednisolone and
+# how many are sensitive? (Hint: you will need to determine
+# which column contains this information; it is one of the last
+# columns)
 
-#####################################################################
-# 5. Find the top differentially expressed probes, with a FDR of 10%,
-# between individuals that are resistant vs. sensitive to prednisolone.
-# Note: there should be 16 probes total. How many of these probes 
-# are up-regulated (i.e., have higher expression) in resistant 
-# individuals and how many are down-regulated (i.e., have lower 
-# expression) in resistant individuals. 
-#####################################################################
 
-########################################################################
-# 6. Construct a heatmap of these top 16 probes, with individuals 
+# 6) Find the most differentially expressed probes, using an FDR of 
+# 10%, for probes that are differentially expressed between 
+# individuals who are resistant and individuals who are sensitive 
+# to prednisolone. Note: there should be 16 probes total. 
+
+# (a) How many of these probes are up-regulated (i.e., have higher
+#     expression) in resistant individuals 
+
+# (b) How many are down-regulated (i.e., have lower expression) 
+#     in resistant individuals. 
+
+# (c) How many of these probes do you expect to be false positives?
+
+
+# 7) Construct a heatmap of these 16 probes, with individuals 
 # color-coded by response to prednisolone (with green=sensitive and 
-# red = resistant). (Note: if you are unable to complete question 5), 
-# you may do this with the first 16 probes in the expression matrix).
-########################################################################
+# red = resistant). For visualizing gene expression values, it is 
+# common to use blue for high expression and yellow for low expression,
+# but you may use any color combination that you wish. Note: if you 
+# are unable to complete question 5), you may do this with the first 
+# 16 probes in the expression matrix.
 
-########################################################################
-# 7. If you answered question 5 correctly, the SECOND hit 
+# 8) If you answered question 6 correctly, the SECOND hit 
 # should be for the probe 209374_s_at. Show that this probe
 # corresponds to the gene IGHM, by first downloading the 
 # correct platform data from GEO, and then finding the gene
 # associated with this probe. 
-#######################################################################
 
 
-#####################################################################
-# 8. How many probes are there for the gene IGHM on the platform
+# 9) How many probes are there for the gene IGHM on the platform
 # in this study? Note: you must search for this gene using the
 # regular expressions covered in the GEO-and-limma.R script. Your 
 # code must also output the number of probes. 
-####################################################################
 
 
 
@@ -113,14 +138,14 @@ res <- t.test(s$Cat, s$Dog, var.equal = TRUE)
 # Final Notes: the heatmap in question 6 provides a candidate list
 # of probes associated with prednisolone response in children with 
 # leukemia. Although much additional work and testing would need to be 
-# done, this kind of gene signature could ultimately be used to 
-# determine whether a child with leukemia would benefit from 
-# prednisolone treatment, or whether an alternative treatment might be 
-# more effective.
+# done to validate these findings, this kind of gene signature could 
+# ultimately be used to determine whether a child with leukemia would 
+# benefit from prednisolone treatment, or whether an alternative 
+# treatment might be more effective.
 
 # The IGHM finding is also interesting. IGHM is a gene that codes
-# for an antibody protein involved in the immune reponse; the 
-# fact that this gene is differentially expressed beween responders and 
-# non-respnoders suggests that a patient's immune system may play a
-# role in how they respond to prednisolone)
+# for an antibody protein that is involved in the immune reponse; the 
+# fact that this gene is differentially expressed between responders 
+# and non-responders suggests that a patient's immune response may 
+# play a role in how well they respond to prednisolone.
 ########################################################################
