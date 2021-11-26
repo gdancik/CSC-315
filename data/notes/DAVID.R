dd92c0e16d73c7dfcec7aa2eca14ba13fbe55ae7
Garrett Dancik <garrett.dancik@gmail.com>
2021-11-26 10:34:15 -0500

    Update

--- data/notes/DAVID.R
+++ data/notes/DAVID.R
@@ -1,130 +1,147 @@
 ###############################################################
-# GPL and DAVID: Getting a list of differentially expressed
-# genes (rather than probes) for DAVID
-###############################################################
-
-################################################################
-# The majority of this script is taken from GEO-and-limma.R,
+# DAVID: Functional analysis of gene sets (gene lists)
+# The majority of this script is taken from knn/limma.R,
 # to identify differentially expressed genes between 
-# males and females
+# males and females. We then provide directions for the
+# functional analysis of these genes
 ################################################################
 
 library(dplyr)
-library(affy)
-library(GEOquery)
+library(ggplot2)
+library(UCSCXenaTools) # needed to retreive data
+library(edgeR) # needed for processing, such as TMM
+library(limma) # needed to find DE probes
 
 
-###########################################################
-# Get the processed data for GSE1297. The object
-# returned by getGEO is a LIST of AffyBatch objects
-# getGEO returns a list because each GEO Series
-# may contain multiple platforms
-###########################################################
+################################################################
+# We first find the top 10 probes differentially expressed
+# between males and females (see limma.R for details)
+################################################################
 
-# Normally use GEOquery, but in class we can use workspace
-# library(GEOquery)
-GSE1297 <- getGEO("GSE1297")
+###################
+# Get the data
+###################
 
-# extract clinical information and gender
-GSE1297.p <- pData(GSE1297[[1]])
-gender <- GSE1297.p$characteristics_ch1.6
+data(XenaData)
 
-# extract gene expression data; for this datset, we must take log2
-GSE1297.expr <- exprs(GSE1297[[1]])
-GSE1297.expr <- log2(GSE1297.expr)
-boxplot(GSE1297.expr, main = "log2 processed data")
+# limit to desired cohort
+blca <- XenaData %>% filter(XenaCohorts == 'GDC TCGA Bladder Cancer (BLCA)')
+
+# Get the phenotype / clinical data
+cli_query = blca %>%
+  filter(Label == "Phenotype") %>%  # select clinical dataset
+  XenaGenerate() %>%  # generate a XenaHub object
+  XenaQuery() %>%     # generate the query
+  XenaDownload()      # download the data
+
+# prepare (load) the data into R
+blca_pheno <- XenaPrepare(cli_query)
+
+# Get the RNA-seq data, including the "probe map"
+cli_query <- blca %>% filter(Label == 'HTSeq - Counts') %>%
+  XenaGenerate() %>%  # generate a XenaHub object
+  XenaQuery() %>%
+  XenaDownload(download_probeMap = TRUE)
+
+# prepare (load) the data into R
+blca_counts <- XenaPrepare(cli_query)
+
+########################################################
+# Pre-process the data to get X (expression matrix),
+# Y (clinical info), and probeMap
+########################################################
+
+# for X, we need to set the rownames and remove the probe column
+# from the data matrix
+X <- data.frame(blca_counts$TCGA.BLCA.htseq_counts.tsv.gz)
+rownames(X) <- X$Ensembl_ID
+X <- X[,-1]  # remove the probe name column
+
+# probeMap = probe names
+probeMap <- blca_counts$gencode.v22.annotation.gene.probeMap
+
+# Y = pheno data
+Y <- blca_pheno
+
+# 'change '.' to '-' so sample ID format is consistent
+colnames(X) <- gsub('\\.', '-', colnames(X))
+
+
+# keep tumor samples ending in 01A
+g <- grep('01A$', colnames(X))
+X <- X[,g]
+
+
+# match expression to clinical data
+common_samples <- intersect(colnames(X), Y$submitter_id.samples)
+mx <- match(common_samples, colnames(X))
+my <- match(common_samples, Y$submitter_id.samples)
+
+X <- X[,mx]
+Y <- Y[my,]
+
+# Make sure that the samples match -- if they don't, this will produce an error
+stopifnot(all(colnames(X) == Y$submitter_id.samples))
+
+
+#################################
+# Process the expression data
+#################################
+
+# convert data from log2 (counts + 1) to counts
+X <- round(2**X - 1)
+
+# first create a Digital Gene Expression (DGE) list object,
+# which contains counts and library size information
+dge <- DGEList(counts=X)
+
+# remove genes with low counts
+keep <- filterByExpr(dge,min.prop = .10 )
+dge <- dge[keep,,keep.lib.sizes=FALSE]
+
+
+# apply TMM normalization, and calculate log CPM
+dge <- calcNormFactors(dge, method = "TMM")
+logCPM <- cpm(dge, log = TRUE, prior.count = 3)
 
 
 ################################################################
 # Find differentially expressed (DE) probes between males
-# and females. 
+# and females
 ###############################################################
-
-library(limma)
 
 # construct design matrix
-design <- model.matrix(~0+gender)
+gender <- Y$gender.demographic
+design <- model.matrix(~-1+gender)
 
-# let's change the column names
+# let's change the column names -- we need to reference them below
 colnames(design) <- c("Female", "Male")
 
-## limma package fits a linear model to each row of the expression matrix ##
-fit <- lmFit(GSE1297.expr, design)
+# 'lmFit' fits a linear model to each row of the expression matrix ##
+fit <- lmFit(logCPM, design)
 
-## Specify the contrasts, which must match column names of design matrix ##
+# Specify the contrasts -- the names here must match column names of 
+# design matrix 
 contrast.matrix <- makeContrasts(Male - Female,levels=design)
 
-## fit model based on contrasts (e.g., Female - Male)
-fit2 <- contrasts.fit(fit, contrast.matrix)
+## fit model based on contrasts (e.g., Male - Female)
+fit <- contrasts.fit(fit, contrast.matrix)
 
-# calculate moderate t-statistics by moderating standard errors
-# toward a common value, which makes answers more robust
-fit2 <- eBayes(fit2)
+# calculate moderated t-statistics by moderating standard errors
+# toward the expected value, using limma trend.
+fit.de <- eBayes(fit, trend = TRUE)
 
-## find all probes differentially expressed at FDR of 5%
-tt <- topTable(fit2,p.value = 0.05, sort.by = "p", number = nrow(GSE1297.expr))
+# get differentially expressed probes using an FDR of 1% 
+tt <- topTable(fit.de,sort.by = "p", p.value = .01, number = Inf)
 tt
 
+##############################################################
+# Get genes for DAVID
+##############################################################
 
-###############################################################
-# Create a heatmap using the top probes (FDR < 0.05)
-###############################################################
-m <- match(rownames(tt), rownames(GSE1297.expr))
-X <- GSE1297.expr[m,]
-col.heat <- colorRampPalette(c("yellow", "blue"))(200)
-
-# set colors for gender #
-col.gender <- as.integer(as.factor(gender))
-col.gender <- c("pink", "blue")[col.gender]
-
-# generate heatmap
-heatmap(X, ColSideColors = col.gender, col = col.heat)
-
-
-###############################################################
-# Let's find the genes associated with all probes
-# This requires using microarray annotations available from
-# GEO (see below) or bioconductor (not discussed) 
-###############################################################
-platform <- annotation(GSE1297[[1]])   
-platform
-
-# download the platform data 
-pl <- getGEO(platform)
-pl <- Table(pl)
-
-##########################################################
-# Next, find the gene for the desired probe 
-# The column names may vary by platform, but for GPL96,
-# the probes are in the ID column and the genes are in 
-# the "Gene Symbol" column
-##########################################################
-probes <- rownames(tt)
-m <- match(probes, pl$ID)
-genes <- pl$`Gene Symbol`[m]
-genes
-##########################################################
-
-##########################################################
-# Starting point for functional analysis -- we have a 
-# list of genes
-##########################################################
-
-# In general, we should remove gene values that are empty strings 
-# (these correspond to probes that do not map to genes)
-keep <- genes!=""
-genes <- genes[keep]
-
-# on this platform, probes corresponding to multiple gene names are separated
-# by " /// ". We can extract all genes using strsplit, which returns a 
-# list
-genes <- strsplit(genes, " /// ")
-
-# using unlist, we can get a vector of all genes
-genes <- unlist(genes)
-
-# get a unique set of genes 
-genes <- unique(genes) 
+m <- match(rownames(tt), probeMap$id)
+genes <- probeMap$gene[m]
+genes <- unique(genes) # get unique set of genes (removes duplicates)
 
 ## view/save genes for input into DAVID (https://david.ncifcrf.gov/)
 ## you may save the genes to a file by setting the file argument,
@@ -140,11 +157,15 @@ write.table(genes, row.names = FALSE, quote = FALSE)
 # upload or paste your gene list;
 
 # Set the identifier to OFFICIAL_GENE_SYMBOL, 
-# Enter your species, 
+# Enter your species (human = Homo sapiens), 
 # Under List Type, select Gene List.
 
-# If necessary, set the List species and Background species 
-# appropiately (in this case to Homo sapiens)
+# Click Submit List
+
+# If < 80% of your genes map, make sure you have selected the right species.
+# If so, continue the analysis with the genes that do  map
+
+# Select to limit annotations by the appropriate species
 
 # Select Functional Annotation Tool
 
@@ -165,6 +186,6 @@ genes <- c("ASF1B", "ASPM", "AURKA", "BIRC5", "BUB1B", "C18orf24",
            "CEP55", "DLGAP5", "DTL", "FOXM1", "KIAA0101", "KIF20A", 
            "MCM10", "NUSAP1", "ORC6L", "PBK", "PRC1", "PTTG1", "RAD51",
            "RAD54L", "RRM2", "TK1", "TOP2A")
-w
+
 
 write.table(genes, row.names = FALSE, quote = FALSE)
