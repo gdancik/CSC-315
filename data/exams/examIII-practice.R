e864f8d33ef5c0c11d6120cdeb25b45a8d07ef93
Garrett Dancik <garrett.dancik@gmail.com>
2021-12-02 22:48:03 -0500

    Update

--- data/exams/examIII-practice.R
+++ data/exams/examIII-practice.R
@@ -9,8 +9,10 @@
 # and/or complete the steps below. 
 
 library(ggplot2)
+library(dplyr)
 
-# 1.Fit a linear model that predicts Petal Width based on the type of flower from
+
+# 1) Fit a linear model that predicts Petal Width based on the type of flower from
 # the iris dataset (see below). Code your explanatory variable 
 # using x = 0 if the sample is of type "versicolor" and x = +1 if the sample is of type 
 # "virginica" (type "setosa" will be ignored). 
@@ -20,7 +22,7 @@ library(ggplot2)
 # (b) Find and interpret the slope in the context of this problem
 # (c) Based on your linear model, what is the p-value for the hypothesis test that there is a
 #     significant difference in Petal Width between the two flower types? 
-#     Note that you can extract the exact p-value from the coefficients object
+#     Note that you should extract the exact p-value from the coefficients object
 #     (which is a matrix) of the fitted linear model. 
 # (d) Verify the p-value from (c) is identical to the p-value obtained from
 #     the two-sample t-test with equal variance
@@ -33,50 +35,55 @@ ggplot(iris, aes(Petal.Length, Petal.Width, color = Species)) +
   theme(legend.background = element_rect(color = "black",
                                          linetype = "solid")) 
 
-# 2.The dataset GSE29561 on the Gene Expression Omnibus (GEO) contains 
-#   gene expression profiles of breast cancer tumors treated by chemotherapy
-#   (epirubicin and docetaxel). Read in the processed data for GSE29561, 
-#   and extract the expression data and phenotype data. Note: this data
-#   is already on the log2 scale. 
+
+
+# 2) The code below reads in the TMM processed logCPM expression data
+#   and pheno data from a dataset comparing patients with 
+#   Alzheimer's Disease to patients who are healthy. Executing the R
+#   code will load two objects into your workspace:
+#
+#  logCPM - the logCPM expression data
+#  pheno  - the phenotype data
+
+# (Note: This dataset was downloaded from: https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE184942)
+
+load(url('https://github.com/gdancik/CSC-315/raw/gh-pages/data/exams/GSE184942.RData'))
+
 #   Answer the following questions based on the expression data:
 #    (a) How many samples are there? 
 #    (b) How many probes are profiled?
-#    (c) What is the mean expression value of the 3rd probe?
+#    (c) What is the mean expression value of the 5th probe?
+#    (d) Which probe has the highest mean expression?
+#        Hint: which.max can be used to find the index of the maximum element of a vector
+
+# 3) In the phenotype data table, characteristics_ch1 indicates whether 
+#    the sample is from a healthy patient or a patient with Alzheimer's Disease (AD).
+#    Generate a relative frequency table for the proportion of patients in each group.
 
 
-# 3. In the phenotype data table, characteristics_ch1.1 indicates whether 
-#    the sample responded to the therapy (treatment response = 1) or 
-#    not (treatment response = 0).  What proportion of samples responded 
-#    to the therapy?
-
-
-
-# 4.  Is the probe 202271_at differentially expressed between samples 
-#     that responded to therapy and samples that did not? 
-#     Construct a boxplot (using ggplot) of the expression of this probe 
-#     across the two groups, and report the fold-change and p-value. 
-
-# 5. Download the appropriate platform data.
-#     (a) How many probes are on this array? 
-#     (b) What gene is associated with the probe 202271_at?
-
-
-# 6.  Using the limma package, find the top differentially expressed probe
+# 4)  Using the limma package, find the top 10 differentially expressed probes
 #     (sorted by p-value). What is the probe name, the fold-change, 
-#     and the adjusted p-value. Is there evidence that this probe is 
-#     differentially expressed? Why or why not? (Note: limma requires that the 
-#     column names of your design matrix NOT be numbers)
+#     and the adjusted p-value of the top probe. 
 
 
-# 7. Based on the probes below, generate a heatmap and color the columns with "darkgreen" for
-#    "treatment response in vivo: 1" and "darkred" for "treatment response in vivo: 0". In 
-#    other words, resistant individuals are dark red and sensitive individuals are dark green
+# 5) Based on the probes below, generate a heatmap and color the columns with "lightgreen" for
+#    healthy samples and darkred for AD samples.
 
-probes = c("201690_s_at","212611_at","218721_s_at",
-           "219463_at","205014_at","216092_s_at",
-           "207375_s_at","202271_at","221551_x_at","219053_s_at")
+probes <- c("ENSG00000171189", "ENSG00000171502", "ENSG00000154080", "ENSG00000240342", 
+    "ENSG00000183044", "ENSG00000226608", "ENSG00000167491", "ENSG00000104490", 
+    "ENSG00000250305", "ENSG00000089169")
 
 
-#8. Using k-nearest neighbor classification with k = 3, find the balanced accuracy 
-#   from a leave-one-out cross-validation for response = 0 vs. response = 1, using the 
-#   probes from problem (7).
+# 6) Using k-nearest neighbor classification with k = 3, calculate the recall for each group, using
+#    the probes from the previous question.
+
+# 7) Show that the probes above correspond to the following genes:
+
+genes <- c("GRIK1", "COL24A1", "CHST9", "RPS2P5", "ABAT", "FTLP3", "GATAD2A", 
+    "NCALD", "KIAA1456", "RPH3A")
+
+
+# 8) Using DAVID, find the top GO term (GOTERM_BP_DIRECT) associated with the genes above.
+# Does this match the summary given for this dataset here: https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE184942
+# Why or why not?
+
