ee37d4070a136ff68b48d1085def3687a8ddb4db
Garrett Dancik <garrett.dancikg@gmail.com>
2022-09-23 15:23:27 -0400

    Update

--- data/notes/AdvancedR.R
+++ data/notes/AdvancedR.R
@@ -33,23 +33,13 @@ divide(3,2) # 3/2 = 1.5
 
 #################################################
 ## for loops exist in R, but there is almost
-## never a good reason to use them
+## never a good reason to use them, and they
+## should not ever be used in this class!
 #################################################
 
-# basic format: iterate over each element in a vector (or list) 
 xvalues <- c(1,3,2,9)
 
-# loop over each value of x
-for (x in xvalues) {
-    cat (x, " ")
-}
-
-# 'traditional approach' to loop over each index value
-for (i in 1:length(xvalues)) {
-  cat (xvalues[i], " ")
-}
-
-# Suppose I want to increase each element of 'x' by 1
+# Suppose I want to increase each element of 'xvalues' by 1
 
 # Using a for loop is not necessary and would be considered
 # bad programming practice
@@ -62,17 +52,14 @@ xvalues
 # But R lets you do this:
 xvalues <- 1:10
 xvalues <- xvalues + 1
+xvalues
 
 ######################################################################
-# Note: For more complex cases, functional approaches 
-#    (apply, lapply, sapply) are generally preferred over loops.
-# Functional approaches are simpler and more readable 
-#     (once understood). 
-#
 # In R, a 'functional' is a function that takes another function
-# as an argument
+# as an input
 #####################################################################
 
+# here we return a vector containing f1(x) and f2(x)
 calc_statistics <- function(x, f1, f2) {
   c(f1(x), f2(x))
 }
@@ -85,9 +72,26 @@ calc_statistics(mydata, mean, median)
 # calculate mean and standard deviation
 calc_statistics(mydata, mean, sd)
 
+
+#####################################################################
+# Instead of loops, the functionals apply, lapply, and sapply
+# should be used to apply a function to each element of a vector or 
+# list. Functional approaches are simpler and more readable, once 
+# understood. 
+
+# apply - applies a function to each row (MARGIN = 1) or column (
+#         MARGIN = 2) of a matrix (or data frame)
+#
+# lapply - applies a function each element of a vector or list, and
+#          returns a list
+
+# sapply - like lapply, but returns a vector if the applied function
+#          returns a single value
+######################################################################
+
 ######################################################################
 ## apply - applies a function to a row (MARGIN = 1) 
-## or column (MARGIN = 2) of a matrix. 
+## or column (MARGIN = 2) of a matrix or data frame. 
 ######################################################################
 
 ###########################################
@@ -114,6 +118,7 @@ ans.col <- apply(m, 2, add.smallest) ## for each col
 ans.col
 
 # additional arguments can be included after the function name
+# (Note: additional arguments should not be called 'x' or 'X')
 ans.row.4 <- apply(m, 1, add.smallest, n = 4)
 ans.row.4
 
@@ -133,8 +138,11 @@ apply(m, 1, function(x) {
 ## another alternative format using an inline (anonymous) function
 apply(m, 1, function(x) sum(sort(x)[1:2]))
 
+
+######################################################################
 ## lapply applies a function to each object in a list or vector, 
 ## and returns a list
+######################################################################
 person <- list(name = "Bob", sibling.ages = c(43,21), pet.ages = c(8,3))
 lapply(person, length)
 
@@ -205,14 +213,15 @@ grades
 #    For example, for v <- c(-3,10,2), min.positive(v) would return 2
 
 # 2. Write a function called min.max which takes a vector x as an argument 
-#    and returns a list containing two named elements, the minimum of x and the
-#    maximum of x. Use this function to find the minimum and maximum of the 
-#    vector below:
+#    and returns a list containing two named elements, 
+#    the minimum of x and the maximum of x. Use this function to find
+#    the minimum and maximum of the vector below:
 
 v <- c(1:10, -13, 90, 2:20)
 
-# 3. Include the following code in your script to create a matrix containing 
-#    the numbers 1 - 30 across 50 columns. Use the 'apply' function
-#    to find the standard deviation of each row
+# 3. Include the following code in your script to create a matrix 
+#    Use the 'apply' function to find the standard deviation of each row
+#    What is the standard deviation of the last row? Why?
 
-m = matrix(1:30, ncol = 5, byrow = TRUE)
+m = matrix(c(1:30, rep(3,5)), ncol = 5, byrow = TRUE)
+
